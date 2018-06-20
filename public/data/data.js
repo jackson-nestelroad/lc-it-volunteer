99375cf89f28d652da7a3bd2b182f7b5df127dc5
Jackson Nestelroad <jacksonroad7@gmail.com>
2018-06-20 13:34:47 -0500

    Typo.

--- public/data/data.js
+++ public/data/data.js
@@ -1,4 +1,4 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: true, // required for Heroku connections
