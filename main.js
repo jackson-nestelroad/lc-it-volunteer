977eadb37e982fec7c321cafa28c7e71c3ac35d3
Jackson Nestelroad <jacksonroad7@gmail.com>
2018-06-20 16:30:27 -0500

    Typo.

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ app.route('/new')
     // registration form is successfully submitted
     .post(function(req, res){
         // req.body contains all of the information we submitted
-        database.checkIfRegistered(req.body.first, req.body.last, req.body.email)
+        database.checkIfRegistered(req.body.name, req.body.email)
         .then(id => {
             // searchByFullName came back with an ID -- volunteer already registered
             if(id){
