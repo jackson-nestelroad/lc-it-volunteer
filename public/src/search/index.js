7ccc8fd5633135343c42194dba84bc184e6d4570
Jackson Nestelroad <jacksonroad7@gmail.com>
2018-07-03 08:41:21 -0500

    Working on search tool.

--- public/src/search/index.js
+++ public/src/search/index.js
@@ -1,10 +1,20 @@
 
+const searchCategories = [
+    'leaderboard',
+    'first',
+    'last',
+    'team',
+    'date',
+    'inactive'
+]
+
 var enter = true;
 
 const back = document.getElementById('backBtn');
 const submit = document.getElementById('searchBtn');
 const close4 = document.getElementById('closeBtn4');
 const select = document.getElementById('category-select');
+const query = document.getElementById('search-query');
 
 close4.addEventListener('click', function(event){
     document.getElementById('httpsqlerror').style['display'] = 'none';
@@ -23,23 +33,40 @@ document.onkeydown = function(evt){
     }
 }
 
-select.addEventListener('load', function(event){
-    // need to search URL and see what category was just searched for and change it to that
-    // basically saving the last search category
-    var category = window.location.search.substr(1);
-})
-
-select.addEventListener('change', function(event){
-    var query = document.getElementById('search-query');
-    var category = this.value;
-    console.log(category);
-    if(category == 1 || category == 6){
+function updateQuery(id){
+    if(id == 1 || id == 6){
         query.disabled = true;
-        query.style['background-color'] = 'rgba(0,0,0,0.075)'
+        query.style['background-color'] = 'rgba(0,0,0,0.075)';
     }
     else{
         query.disabled = false;
         query.style['background-color'] = 'white';
+    }
+}
+
+window.onload = function(){
+    // need to search URL and see what category was just searched for and change it to that
+    // basically saving the last search category
+    var category = window.location.search.substr(1).substr(0, window.location.search.substr(1).indexOf('='));
+    category = searchCategories.indexOf(category) + 1;
+    if(category == 0){
+        select.value = 1;
+        updateQuery(1);
+    }
+    else{
+        select.value = category;
+        updateQuery(category);
+    }
+    query.value = window.location.search.substr(1).substr(window.location.search.substr(1).indexOf('=')+1);
+}
+
+select.addEventListener('change', function(event){
+    var category = this.value;
+    if(category == 1 || category == 6){
+        updateQuery(category);
+    }
+    else{
+        updateQuery(category);
     }
 })
 
