5a95d5d5ffe3beaf5c421f38c095aeb721bdbff4
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2018-07-02 21:49:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ app.route('/new')
         });
     })
 // /data is database page with leaderboard, search, and inactive list
-app.get('/data', function(req, res){
+app.get('/search', function(req, res){
     res.sendFile(__dirname + '/public/src/search/index.html');
 })
 // /display is the display for TV
