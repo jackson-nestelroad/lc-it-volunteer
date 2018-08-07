c2735806d1903d6a857f6f83f889756899007726
Jackson Nestelroad <jacksonroad7@gmail.com>
2018-08-07 13:12:42 -0500

    Fix staff display.

--- public/data/data.js
+++ public/data/data.js
@@ -1464,7 +1464,7 @@ exports.getStaff = function(){
                     staff
                     FROM logs
                     WHERE staff IS NOT NULL
-                    AND date >= '${month-1}/1/${year}'
+                    AND date >= '${month}/1/${year}'
                     GROUP BY staff)
                 SELECT staff,
                 volunteers,
